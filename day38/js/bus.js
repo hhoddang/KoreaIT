776e5299b705cde70d7409c581cd17a45653521d
김호찬 <melons98@naver.com>
2023-05-30 13:50:32 +0900

    commit

--- day38/js/bus.js
+++ day38/js/bus.js
@@ -44,14 +44,74 @@ let stationList = [
   "창동고등학교",
 ];
 
+var intervalDistance = 1250; // 정류장사이 고정거리
+
 //버스요금계산함수
 function calculator() {
   var start = document.getElementById("start").value;
   var end = document.getElementById("end").value;
   var result = document.getElementById("result");
+  var type = document.querySelector("[name=people]:checked").value;
 
-  //   result.innerHTML = `<p>출발정거장은   ${stationList[start]}  이고    도착정거장은  ${stationList[end]}  입니다 </p>`;
-  result.insertAdjacentHTML("afterbegin" , `<p>출발정거장은   ${stationList[start]}  이며   도착정거장은  ${stationList[end]}  입니다 </p>`);
+  //   alert(start); // 배열의 index 자리 출력
+  //   alert(end);
+  //   alert("stationList.length:" + stationList.length);
+
+  if (Number(start) == Number(end)) {
+    result.insertAdjacentHTML(
+      "afterbegin",
+      `<p> 출발 정거장과 도착 정거장이 같습니다. </p>`
+    );
+  }
+
+  if (Number(start) > Number(end)) {
+    //출발 정류장이 도착 정류장보다 앞에 있을 시.
+    stationCount = stationList.length - Number(start) + Number(end);
+  } else {
+    //출발 정류장이 도착정류장보다 뒤에 있을시
+    stationCount = Number(end) - Number(start);
+  }
+  var distance = stationCount * intervalDistance;
+  var cost = busCost(distance, type);
+  //출발 정류장과 도착 정류장 사이의 거리
+  //총정거장  이동거리는 =  이동거리 x 정거장간의 고정거리
+  result.insertAdjacentHTML(
+    "afterbegin",
+    `<p>출발정거장은   ${stationList[start]}  이며   도착정거장은  ${stationList[end]}  입니다. </p>`
+  );
+
+  result.insertAdjacentHTML(
+    "afterbegin",
+    `<p> 총 ${stationCount} 개의 정류장을 거쳤습니다. 이동거리는 ${distance} m 입니다.</p>`
+  );
+
+  var stationCount = 0; // 정류장간의 정류장수에 대한 초기화
+  result.insertAdjacentHTML(
+    "afterbegin",
+    `<p> 버스요금은 \ ${cost}원  입니다.</p>`
+  );
+}
+
+//비용을 계산하는 함수
+function busCost(distance, type) {
+  var baseAmout =
+    type == "adult" ? 1250 : type == "young" ? 720 : type == "child" ? 450 : 0;
+
+  if (distance <= 10000) {
+    // 10km 이내일 경우 기본요금
+    return baseAmout;
+  } else if (distance <= 30000) {
+    // 10km ~ 30km 이내라면 5km마다 100원 추가
+    return baseAmout + Math.ceil((distance - 10000) / 5000) * 100;
+    //Math.ceil();
+  } else {
+    //30km 이상이라면 3km 마다 100원 추가
+    return (
+      baseAmout +
+      (20000 / 5000) * 100 +
+      Math.ceil((distance - 30000) / 3000) * 100
+    );
+  }
 }
 
 //웹브라우저에 html이 처음 띄워졌을때
