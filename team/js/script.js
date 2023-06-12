c6067b187516bfc0c29a44ec0727f102925fdb05
김호찬 <melons98@naver.com>
2023-06-12 13:50:41 +0900

    commit

--- team/js/script.js
+++ team/js/script.js
@@ -44,6 +44,7 @@ if (w < 599) {
     });
   });
 
+  //  === 모바일 슬라이드 ====
   var mbox = 1;
   function next(x) {
     mbox++;
@@ -56,10 +57,30 @@ if (w < 599) {
   function prev(x) {
     mbox--; //1씩 증가
     if (mbox == 0) {
-      
-      mbox = 1; 
+      mbox = 1;
     }
     document.getElementById("Mobilemainimg").src =
       "images/m_main" + mbox + ".jpg";
   }
 }
+
+var pw = $(window).width();
+if (w < 767 && w > 600) {
+  var tbox = 1;
+  function next(x) {
+    tbox++;
+    if (tbox == 3) {
+      tbox = 1;
+    }
+    document.getElementById("tabletmainimg").src = "images/tablet_bg" + tbox + ".jpg";
+  }
+  function prev(x) {
+    tbox--; //1씩 증가
+    if (tbox == 0) {
+      //변수 box의 값이 5보다 크게되면
+      tbox = 1; //변수 box 재선언한다(파일명에 1이 들어간 이미지를 보이게 한다)
+    }
+    document.getElementById("tabletmainimg").src = "images/tablet_bg" + tbox + ".jpg";
+  }
+}
+// ==== 태블릿 슬라이드 ====
